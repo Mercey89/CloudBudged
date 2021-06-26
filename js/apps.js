7359c7e7e8e4dcf1a8836c922e03f3651047c74d
Mercey <main.mercey@gmail.com>
2021-06-26 17:42:27 +0100

    slide

--- js/apps.js
+++ js/apps.js
@@ -47,3 +47,71 @@ $(document).ready(function() {
 		})
 	})
 })
+
+// Slider (focus)
+$(document).ready(function() {
+	let sliderItem = $(".slider").children(".slider__item.active");
+	sliderItem.prev(".slider__item").css({
+		"left":-35 +'%',
+	});
+
+	sliderItem.next(".slider__item").css({
+		"right":-35 +'%',
+	});
+	let i = $(".slider").children(".slider__item");
+	let ind=0;
+	$(".slider").children('.slider__item').each(function(){
+		$(this).attr('data-index',ind++);
+
+	})
+	i.on('click',function(e){
+		const that = $(this);
+		let dataIndex = that.data('index');
+		$(".slider__item").removeClass('active');
+		that.addClass('active');
+		i.each(function(){
+			if($(this).data('index')==dataIndex){
+				$(this).addClass('active');
+				$(this).css({
+					"left":0,
+					"right":0,
+					"z-index":3,
+				});
+				if(dataIndex=="1"){
+					$(".slider__item[data-index=2]").css({
+						"left":0,
+						"right":-35 +'%',
+						"z-index":1,
+					})
+					$(".slider__item[data-index=0]").css({
+						"left":-35 +'%',
+						"right":0,
+						"z-index":1,
+					})
+				}else if(dataIndex=="0"){
+					$(".slider__item[data-index=2]").css({
+						"left":-35 +'%',
+						"right":0,
+						"z-index":1,
+					})
+					$(".slider__item[data-index=1]").css({
+						"left":0,
+						"right":-35 +'%',
+						"z-index":1,
+					})
+				}else if(dataIndex=="2"){
+					$(".slider__item[data-index=1]").css({
+						"left":-35 +'%',
+						"right":0,
+						"z-index":1,
+					})
+					$(".slider__item[data-index=0]").css({
+						"left":0,
+						"right":-35 +'%',
+						"z-index":1,
+					})
+				}
+			}
+		})
+	})
+})
